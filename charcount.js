d106263b5fed6d69458a88f408a75ac16753e797
Rose Wan <rwan@mail.com>
2017-05-31 15:33:55 +0000

    char count

--- charcount.js
+++ charcount.js
@@ -7,9 +7,9 @@ function countLetters (str) {
 
 	for (var i = 0; i < letter.length; i++) {
 		if (output[letter[i]]){
-			output[letter[i]]++;
+			output[letter[i]] += ", " + i.toString();
 		} else {
-			output[letter[i]] = 1;
+			output[letter[i]] = i.toString();
 		}
 	 } 
 	 return output;
@@ -17,4 +17,22 @@ function countLetters (str) {
 
 console.log(countLetters(param));
 
-//{ l: 1, i: 2, g: 1, h: 4, t: 2, o: 2, u: 2, s: 2, e: 3, n: 1 }
+/*l: 0,
+i: 1, 10,
+g: 2,
+h: 3, 5, 13, 15,
+t: 4, 12,
+o: 6, 
+
+{ l: '0',
+  i: '1, 10',
+  g: '2',
+  h: '3, 5, 13, 15',
+  t: '4, 12',
+  o: '6, 16',
+  u: '7, 17',
+  s: '8, 18',
+  e: '9, 14, 19',
+  n: '11' }
+
+*/
